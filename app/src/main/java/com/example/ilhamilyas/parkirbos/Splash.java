9585a68d97818d597a56d9b327a6d2fe7461eedd
DESKTOP-II9ABME\ilham ilyas <ilhamilyas100@gmail.com>
2018-03-09 01:50:03 -0800

    commit

---
+++ app/src/main/java/com/example/ilhamilyas/parkirbos/Splash.java
@@ -0,0 +1,33 @@
+package com.example.ilhamilyas.parkirbos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+    private  static  final  int splashInterval = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i =new Intent(Splash.this, LoginActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+                finish();
+            }
+        },splashInterval);
+    }
+
+}
---
+++ app/src/main/java/com/example/ilhamilyas/parkirbos/splashActivity.java
@@ -0,0 +1,14 @@
+package com.example.ilhamilyas.parkirbos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class splashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+
+    }
+}
